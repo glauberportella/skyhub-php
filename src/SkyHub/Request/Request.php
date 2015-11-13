d9e55884440c2240ad8cfb74832d853e0c4d16b8
macweb <macweb@macweb.com.br>
2015-11-13 17:57:09 -0200

    classes definitions, uml model update and composer.json created

---
+++ src/SkyHub/Request/Request.php
@@ -0,0 +1,85 @@
+<?php
+// Copyright (c) 2015 Glauber Portella <glauberportella@gmail.com>
+
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+namespace SkyHub\Request;
+
+abstract class Request implements RequestInterface
+{
+	/**
+	 * @var integer
+	 */
+	protected $page;
+
+	/**
+	 * @var integer
+	 */
+	protected $perPage;
+
+	public function send();
+
+    /**
+     * Gets the value of page.
+     *
+     * @return integer
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Sets the value of page.
+     *
+     * @param integer $page the page
+     *
+     * @return self
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    /**
+     * Gets the value of perPage.
+     *
+     * @return integer
+     */
+    public function getPerPage()
+    {
+        return $this->perPage;
+    }
+
+    /**
+     * Sets the value of perPage.
+     *
+     * @param integer $perPage the per page
+     *
+     * @return self
+     */
+    public function setPerPage($perPage)
+    {
+        $this->perPage = $perPage;
+
+        return $this;
+    }
+}
