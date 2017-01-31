24c9988d789436f1b3e3e8a33d22b9f2ccb317b7
Glauber Portella <glauberportella@gmail.com>
2017-01-31 21:24:39 -0200

    tested delete from queue

--- tests/Unit/SkyHub/Request/QueueRequestTest.php
+++ tests/Unit/SkyHub/Request/QueueRequestTest.php
@@ -24,4 +24,14 @@ class QueueRequestTest extends \PHPUnit_Framework_TestCase
 		file_put_contents(dirname(__FILE__).'/../../../data/queue.log', json_encode($orders, JSON_PRETTY_PRINT));
 		$this->assertTrue(true);
 	}
+
+	public function testQueueRequestDelete()
+	{
+		$queue = new \SkyHub\Request\QueueRequest($this->auth);
+		$orders = $queue->orders();
+		$queue->delete($orders->code);
+		$fromQueue = $queue->orders();
+		file_put_contents(dirname(__FILE__).'/../../../data/queue-delete.log', json_encode($fromQueue, JSON_PRETTY_PRINT));
+		$this->assertTrue(true);
+	}
 }
