02280f49c6f41d83a4750736265ef10a02f8b984
Glauber Portella <glauberportella@gmail.com>
2017-03-13 07:23:58 -0300

    curl request

--- src/SkyHub/Request/Request.php
+++ src/SkyHub/Request/Request.php
@@ -87,7 +87,7 @@ abstract class Request implements RequestInterface
             CURLOPT_TIMEOUT => RequestInterface::TIMEOUT,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_HTTPHEADER => array(
-                'accept: application/json',
+                'accept: application/json, application/gzip, application/x-gzip',
                 'content-type: application/json',
                 'x-api-key: '.$this->auth->getToken(),
                 'x-user-email: '.$this->auth->getEmail(),
